b1c38c3b9499651e599f9f31d21013b998cc58f2
Stephen Wong <wongstephenky@gmail.com>
2023-03-29 12:58:31 -0700

    switched to netlify form

--- main.js
+++ main.js
@@ -14,25 +14,6 @@ hiddenElements.forEach((el) => {
   observer.observe(el);
 });
 
-const handleSubmit = (event) => {
-  event.preventDefault();
-
-  const myForm = event.target;
-  const formData = new FormData(myForm);
-
-  console.log(formData);
-
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => console.log("Form successfully submitted"))
-    .catch((error) => alert(error));
-};
-
-document.querySelector("form").addEventListener("submit", handleSubmit);
-
 const date = new Date().getFullYear();
 const footer = document.querySelector("footer");
 footer.innerText += " " + date;
