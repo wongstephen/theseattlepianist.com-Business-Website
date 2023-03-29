c40fe040a530f985fa01ef30856367d5215d7d48
Stephen Wong <wongstephenky@gmail.com>
2023-03-29 12:51:22 -0700

    fixed form

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ const handleSubmit = (event) => {
   const myForm = event.target;
   const formData = new FormData(myForm);
 
-  fetch("https://shawnschlogelmusic.netlify.app/", {
+  fetch("/", {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: new URLSearchParams(formData).toString(),
