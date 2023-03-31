da2a63d0094ad04f37de1578bf0e79879b48ab27
Stephen Wong <wongstephenky@gmail.com>
2023-03-31 10:29:10 -0400

    removed listeners for title

--- main.js
+++ main.js
@@ -7,8 +7,7 @@ class Observer {
   }
   getHiddenElements() {
     const hiddenElements = document.querySelectorAll("." + this._hiddenClass);
-
-    console.log(this._hiddenClass);
+    console.log(hiddenElements);
     //returns arr of hidden elements
     return hiddenElements;
   }
@@ -19,7 +18,7 @@ class Observer {
         if (entry.isIntersecting) {
           entry.target.classList.add(this._showClass);
         } else {
-          entry.target.classList.remove(this._showClass);
+          // entry.target.classList.remove(this._showClass);
         }
       });
     });
@@ -27,11 +26,16 @@ class Observer {
       observer.observe(el);
     });
   }
+  test() {
+    console.log("hi");
+  }
 }
+
 const mainImg = new Observer("show-bottom", "hidden-bottom");
 mainImg.addObserver();
-const title = new Observer("show-title", "hidden-title");
-title.addObserver();
+
+// const title = new Observer("show-title", "hidden-title");
+// title.addObserver();
 
 const date = new Date().getFullYear();
 const footer = document.querySelector("footer");
