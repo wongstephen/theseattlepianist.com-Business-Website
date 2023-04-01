13f64065961ea55f3707aa6381670a99941eee75
Stephen Wong <wongstephenky@gmail.com>
2023-04-01 09:55:51 -0400

    changed oberserver to classes

--- main.js
+++ main.js
@@ -1,18 +1,37 @@
 import "./style.css";
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    } else {
-      // entry.target.classList.remove("show");
-    }
-  });
-});
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => {
-  observer.observe(el);
-});
+class Observer {
+  constructor(showClass, hiddenClass) {
+    this._showClass = showClass;
+    this._hiddenClass = hiddenClass;
+  }
+  getHiddenElements() {
+    const hiddenElements = document.querySelectorAll("." + this._hiddenClass);
+
+    console.log(this._hiddenClass);
+    //returns arr of hidden elements
+    return hiddenElements;
+  }
+  addObserver() {
+    const hiddenElements = this.getHiddenElements();
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add(this._showClass);
+        } else {
+          entry.target.classList.remove(this._showClass);
+        }
+      });
+    });
+    hiddenElements.forEach((el) => {
+      observer.observe(el);
+    });
+  }
+}
+const mainImg = new Observer("show-bottom", "hidden-bottom");
+mainImg.addObserver();
+const title = new Observer("show-title", "hidden-title");
+title.addObserver();
 
 const date = new Date().getFullYear();
 const footer = document.querySelector("footer");
